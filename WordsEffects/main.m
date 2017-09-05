b94f1092d43d10b052ce95a418887f154d960252
Jaison Bhatti <jaison.bhatti@gmail.com>
2017-09-04 20:37:02 -0400

    capitalize

--- WordsEffects/main.m
+++ WordsEffects/main.m
@@ -8,17 +8,24 @@
 
 #import <Foundation/Foundation.h>
 
+void displayMenu() {
+    printf ("Enter a number below: \n");
+    printf ("1. UPPERCASE \n");
+    printf ("2. Lowercase \n");
+    printf ("3. Numberize \n");
+    printf ("4. Canadianize \n");
+    printf ("5. Respond \n");
+    printf ("6. De-Space-it \n");
+    printf ("0. Finished \n");
+}
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        
-        //int inputNum;
-        //printf("Input a number: ");
-        //fgets(inputNum, 7, stdin);
-        
+        // insert code here...
         // 255 unit long array of characters
         char inputChars[255];
         
-        printf("Input a String: ");
+        printf("Input a string: ");
         // limit input to max 255 characters
         fgets(inputChars, 255, stdin);
         
@@ -29,10 +36,36 @@ int main(int argc, const char * argv[]) {
         NSString *inputString = [NSString stringWithUTF8String:inputChars];
         
         // print NSString object
-        NSLog(@"Input was: %@", inputString);
+        NSLog(@"Input was: %@\n", inputString);
         
         
-        
+        int choice;
+        do{
+            displayMenu();
+            
+            
+            scanf ("%i", &choice);
+            switch (choice) {
+                
+                case 0:
+                    NSLog (@"Thank you for playing!");
+                    break;
+                
+                case 1: {
+                    NSString *capitalLetters = [inputString uppercaseString];
+                    NSLog(@"Input was: %@", capitalLetters);
+                    
+                    break;
+                }
+                //
+                    //
+                default:
+                    NSLog (@"Integer out of range");
+                    break;
+            }
+            
+        }
+        while(choice!=0);
         
     }
     return 0;
